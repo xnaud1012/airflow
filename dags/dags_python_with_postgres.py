354c3bd7843f877a04335e62362769f87f91b0f5
xnaud <tmdtp314@naver.com>
2023-12-06 13:46:48 +0900

    connection

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -3,7 +3,7 @@ import pendulum
 from airflow.operators.python import PythonOperator
 
 with DAG(
-        dag_id='dags_python_with_postgres_hook',
+        dag_id='dags_python_with_postgres',
         start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
         schedule=None,
         catchup=False
