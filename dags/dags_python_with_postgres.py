c79516e349e78351aa6118dc07d66375c5351ff0
xnaud <tmdtp314@naver.com>
2023-12-06 10:46:15 +0900

    asdf2

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -27,7 +27,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.3', '5432', 'xnaud', 'xnaud', 'xnaud']
+        op_args=['172.28.0.3', '5432', 'xnaud', 'cnuh', 'xnaud']
     )
         
     insrt_postgres
