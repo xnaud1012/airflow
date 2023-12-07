0a54072519b42ff753b0359f3c33890c101433fb
xnaud <tmdtp314@naver.com>
2023-12-07 15:01:35 +0900

    error

--- dags/dags_python_with_oracle.py
+++ dags/dags_python_with_oracle.py
@@ -1,3 +1,4 @@
+'''
 from airflow import DAG
 import pendulum
 from airflow.decorators import task
@@ -24,3 +25,36 @@ with DAG(
 ) as dag:
     data = get_data_from_oracle()
     insert_data_into_postgres(data)
+'''
+
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        dag_id='dags_python_with_postgres',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule=None,
+        catchup=False
+) as dag:
+    def insrt_postgres(postgres_conn_id, **kwargs):
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
+        from contextlib import closing
+        
+        postgres_hook = PostgresHook(postgres_conn_id)
+        with closing(postgres_hook.get_conn()) as conn:
+            with closing(conn.cursor()) as cursor:
+                dag_id = kwargs.get('ti').dag_id
+                task_id = kwargs.get('ti').task_id
+                run_id = kwargs.get('ti').run_id
+                msg = 'hook insrt 수행'
+                sql = 'insert into test values (%s,%s,%s,%s);'
+                cursor.execute(sql, (dag_id, task_id, run_id, msg))
+                conn.commit()
+
+    insrt_postgres_with_hook = PythonOperator(
+        task_id='insrt_postgres_with_hook',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id':'conn-db-postgres-custom'}
+    )
+    insrt_postgres_with_hook
