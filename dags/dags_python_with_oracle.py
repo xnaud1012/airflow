daa3295c312e3971fd5c1714cde059eae187295b
xnaud <tmdtp314@naver.com>
2023-12-08 14:30:08 +0900

    oraclehook

--- dags/dags_python_with_oracle.py
+++ dags/dags_python_with_oracle.py
@@ -4,33 +4,17 @@ import pendulum
 from airflow.decorators import task
 from airflow.providers.oracle.hooks.oracle import OracleHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-import cx_Oracle
 
 
 table_name="test"
 @task
 def get_data_from_oracle():
-    # Oracle 연결 정보 설정
-    oracle_conn_str = "username/password@hostname:port/service_name" ##
-    connection = cx_Oracle.connect(oracle_conn_str)
-
-    # SQL 쿼리 작성
-    sql_query = "SELECT * FROM your_oracle_table"
-
-    # 쿼리 실행
-    cursor = connection.cursor()
-    cursor.execute(sql_query)
-
-    # 데이터 가져오기
-    data = cursor.fetchall()
-
-    print(data)
-
-    # 연결 닫기
-    cursor.close()
-    connection.close()
-
+    oracle_hook = OracleHook('oracle_default')
+    data =oracle_hook.get_pandas_df(sql=f'SELECT * FROM {table_name}')
     return data.to_dict()
+    # Oracle 연결 정보 설정
+
+
 
 @task
 def insert_data_into_postgres(data):
