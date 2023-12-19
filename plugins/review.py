7811ae8988c9181e45be9f4efa3db8aed43d1b8c
xnaud <tmdtp314@naver.com>
2023-12-19 13:25:45 +0900

    sd

--- plugins/review.py
+++ plugins/review.py
@@ -7,7 +7,7 @@ import psycopg2
 bp = Blueprint(
                "review_plugin",
                __name__,
-               static_folder='static',
+               static_folder='static/DataTables',
                template_folder="templates" # registers airflow/plugins/templates as a Jinja template folder
                )
 
