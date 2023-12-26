3728c3967ac94c188ceef13da1bc4c064f53240c
xnaud <tmdtp314@naver.com>
2023-12-26 16:18:20 +0900

    s

--- plugins/static/sql/update.sql
+++ plugins/static/sql/update.sql
@@ -1,6 +1,5 @@
 UPDATE test
-SET test_id = %(test_id)s, 
-    test_a = %(test_a)s, 
+SET test_a = %(test_a)s, 
     test_b = %(test_b)s, 
     test_c = %(test_c)s
-WHERE test_id = 'admin1'
+WHERE test_id = %(test_id)s
