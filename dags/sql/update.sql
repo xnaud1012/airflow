8db17765f564ca71b8d2d94da8e1928dc046ab26
xnaud <tmdtp314@naver.com>
2024-01-08 10:54:55 +0900

    g

--- dags/sql/update.sql
+++ dags/sql/update.sql
@@ -1,3 +1,3 @@
-INSERT INTO test (test_a, test_b, test_c, test_d)
-VALUES (%(test_a)s,  %(test_b)s,  %(test_c)s, %(test_d)s)
+INSERT INTO test (test_id, test_a, test_b, test_c)
+VALUES (%(test_id)s,  %(test_a)s,  %(test_b)s, %(test_c)s)
 
