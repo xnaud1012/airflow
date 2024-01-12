d98904fe589be247a5b68501d647692da7ae9506
xnaud <tmdtp314@naver.com>
2024-01-12 14:40:32 +0900

    S

--- dags/dag_branch_python_operator_example.py
+++ dags/dag_branch_python_operator_example.py
@@ -9,7 +9,7 @@ with DAG(
     schedule='0 1 * * *',
     catchup=False
 ) as dag:
-    def select_random(): #return 값은 task 이름(객체)이어야 함.
+    def select_random(): #return 값은 task id이어야 함.
         import random
 
         item_lst = ['A','B','C']
@@ -21,7 +21,7 @@ with DAG(
     
     python_branch_task = BranchPythonOperator(
         task_id='python_branch_task',
-        python_callable=select_random # retun 받은 task 명 
+        python_callable=select_random # retun 받은 task id
     )
     
     def common_func(**kwargs):
@@ -46,3 +46,50 @@ with DAG(
     )
 
     python_branch_task >> [task_a, task_b, task_c] # python_branch_task 라는 태스크가 선행 태스크, 뒤의 []안에 있는 태스크들이 후행 태스크 
+
+    '''
+    from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id='dags_python_with_branch_decorator',
+    start_date=datetime(2024,1,1),
+    schedule=None,
+    catchup=False
+) as dag:
+    @task.branch(task_id='python_branch_task')
+    def select_random():
+        import random
+        item_lst = ['A', 'B', 'C']
+        selected_item = random.choice(item_lst)
+        if selected_item == 'A':
+            return 'task_a'
+        elif selected_item in ['B','C']:
+            return ['task_b','task_c']
+    
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )
+
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
+    )
+
+    task_c = PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    )
+
+    select_random() >> [task_a, task_b, task_c]
+    
+    '''
