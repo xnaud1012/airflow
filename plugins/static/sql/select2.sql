9eab1a25f4a38b971546c056feff6f381f7dadd2
xnaud <tmdtp314@naver.com>
2024-01-17 16:21:17 +0900

    S

--- plugins/static/sql/select2.sql
+++ plugins/static/sql/select2.sql
@@ -1,6 +1,5 @@
-
 WITH CTE AS(
-SELECT SUBSTR(json_agg, 1, LENGTH(json_agg) - 1) AS HEAD_INFO
+SELECT '['|| SUBSTR(json_agg, 1, LENGTH(json_agg) - 1) ||'}]'AS HEAD_INFO
 FROM (
     SELECT LISTAGG(XNAUD.JSON_OBJECT(column_name, comments), ',')
     WITHIN GROUP (ORDER BY column_name) AS json_agg
@@ -10,4 +9,5 @@ FROM (
 )
 SELECT cte.HEAD_INFO, m.* FROM CTE
 INNER JOIN 
-MSEPMRID m ON 1=1 
+MSEPMRID m ON 1=1 ;
+
