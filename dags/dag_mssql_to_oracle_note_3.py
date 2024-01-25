ba103b88919cc44e03bf43fd4aa1c3b371b9b0f8
xnaud <tmdtp314@naver.com>
2024-01-25 10:35:50 +0900

    S

---
+++ dags/dag_mssql_to_oracle_note_3.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, pendulum
+from airflow import DAG
+from airflow.providers.jdbc.operators.jdbc import JdbcOperator
+import logging
+#데이터프레임 이용해서 SQL수행 
+
+with DAG(
+    dag_id='jdbc_bridge_mssql',
+    start_date=pendulum.datetime(2024, 1, 1, tz='Asia/Seoul'),
+    #schedule="*/2 * * * *",
+    schedule='0 0 * * *',
+    catchup=False
+) as dag:
+    
+
+    jdbc_task = JdbcOperator(
+    task_id='run_sql_query',
+    sql='SELECT * FROM DEATH',
+    jdbc_conn_id='MSSQL_JDBC_CONN',  # Airflow에서 설정한 연결 ID
+    dag=dag,
+    )
+
+    jdbc_task 
