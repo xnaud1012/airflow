1c47780800fa7a48c00695078ed9e672d8ec11b8
xnaud <tmdtp314@naver.com>
2024-01-25 10:57:04 +0900

    S

--- dags/dag_mssql_to_oracle_note_3.py
+++ dags/dag_mssql_to_oracle_note_3.py
@@ -1,28 +1,20 @@
-from datetime import datetime, timedelta
+
 import pendulum
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.providers.jdbc.hooks.jdbc import JdbcHook
-import logging
-
-# 데이터프레임 이용해서 SQL수행 
-
-def my_custom_function(**kwargs):
-    jdbc_hook = JdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
-    records = jdbc_hook.get_records("SELECT * FROM DEATH")
-    for record in records:
-        print(record)
-
+from airflow.decorators import task
 with DAG(
     dag_id='jdbc_bridge_mssql',
     start_date=pendulum.datetime(2024, 1, 1, tz='Asia/Seoul'),
     schedule_interval='0 0 * * *',
     catchup=False
 ) as dag:
+    @task(task_id='execute')
+    def my_custom_function(**kwargs):
+        jdbc_hook = JdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
+        records = jdbc_hook.get_records("SELECT * FROM DEATH")
+        for record in records:
+            print(record)
 
-    run_my_query = PythonOperator(
-        task_id='task1',
-        python_callable=my_custom_function
-    )
 
-run_my_query
+my_custom_function()
