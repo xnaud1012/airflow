1f5e2ccec7c612dd2a922bd9ddecba8d83868c48
xnaud <tmdtp314@naver.com>
2024-01-25 14:29:15 +0900

    S

--- dags/dag_mssql_to_oracle_note_3.py
+++ dags/dag_mssql_to_oracle_note_3.py
@@ -3,24 +3,25 @@ import pendulum
 from airflow import DAG
 from airflow.providers.jdbc.hooks.jdbc import JdbcHook
 from airflow.decorators import task
+
+
+
 with DAG(
     dag_id='jdbc_bridge_mssql',
     start_date=pendulum.datetime(2024, 1, 1, tz='Asia/Seoul'),
-    schedule_interval='0 0 * * *',
+    #schedule="*/2 * * * *",
+    schedule='0 0 * * *',
     catchup=False
 ) as dag:
-    @task(task_id='execute2')
-    def execute2(**kwargs):
-        print('start')
+    @task(task_id='execute')
+    def execute(**kwargs):
         jdbc_hook = JdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
+        print(jdbc_hook)
         records = jdbc_hook.get_records("SELECT * FROM DEATH")
-        asdf=""
-        for record in records:
-            asdf += ' '.join(map(str, record)) + "\n"
+        print(records)
+        if records == None:
+            records="sssssss"
+        return records
 
-        
+    execute()
 
-        return asdf
-
-
-execute2()
