8e2e0162b63c7ef0553b7c53e41c966773744357
xnaud <tmdtp314@naver.com>
2024-01-26 09:45:02 +0900

    S

--- dags/jbdc_bridge_mssql.py
+++ dags/jbdc_bridge_mssql.py
@@ -4,6 +4,9 @@ from airflow import DAG
 from airflow.providers.jdbc.hooks.jdbc import JdbcHook
 from airflow.decorators import task
 import logging
+import jpype
+import jpype.imports
+from jpype.types import *
 
 with DAG(
     dag_id='jdbc_bridge_mssql',
@@ -14,6 +17,12 @@ with DAG(
 ) as dag:
     @task(task_id='execute')
     def execute(**kwargs):
+        print('start')
+        if not jpype.isJVMStarted():
+            jpype.startJVM()
+
+        Driver = jpype.JClass('com.your.jdbc.DriverClassName')
+        print(Driver)
         jdbc_hook = JdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
         print(jdbc_hook)
         records = jdbc_hook.get_records("SELECT * FROM DEATH")
