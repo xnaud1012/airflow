fe22493f1a9ebd748edc6aef92e9ea56b4d2dd0f
xnaud <tmdtp314@naver.com>
2024-01-26 11:08:21 +0900

    S

--- dags/jbdc_bridge_mssql.py
+++ dags/jbdc_bridge_mssql.py
@@ -1,67 +1,24 @@
-
 import pendulum
 from airflow import DAG
 from airflow.providers.jdbc.hooks.jdbc import JdbcHook
 from airflow.decorators import task
-import logging
-import jpype
-import jpype.imports
-from jpype.types import *
 
 with DAG(
     dag_id='jdbc_bridge_mssql',
     start_date=pendulum.datetime(2024, 1, 1, tz='Asia/Seoul'),
-    #schedule="*/2 * * * *",
     schedule='0 0 * * *',
     catchup=False
 ) as dag:
-    
-
-    class LoggingJdbcHook(JdbcHook):
-        def get_conn(self):
-            # Airflow Connection 객체에서 connection 정보를 가져옵니다.
-            conn = self.get_connection(getattr(self, self.conn_name_attr))
-            conn_config = conn.extra_dejson
-            
-            # 'Extra' 필드에서 드라이버 클래스 이름(jclassname)을 추출합니다.
-            jclassname = conn_config.get('driver', None)
-            
-            # jclassname 값을 로깅합니다.
-            logging.info(f"JdbcHook.get_conn() - Driver class name: {jclassname}*********************************************************!!!!!!!!!!!!!!!!!!!!!!")
-
-            # JDBC Hook의 원래 get_conn() 메서드를 호출하여 Connection 객체를 반환합니다.
-            return super().get_conn()
-    
 
     @task(task_id='execute')
     def execute(**kwargs):
-        print('start')
-        if not jpype.isJVMStarted():
-            jpype.startJVM()
-
-        java_class_path = jpype.java.lang.System.getProperty("java.class.path")
-        print(f"Java Class Path: {java_class_path}")
-
-        Driver = jpype.JClass('com.microsoft.sqlserver.jdbc.SQLServerDriver')
-        jpype.java.sql.DriverManager.registerDriver(Driver())
-
-       
-        url = 'jdbc:sqlserver://10.91.102.97:1433;databaseName=xnaud;user=sa;password=johnson0102;trustServerCertificate=true;'
-        conn = jpype.java.sql.DriverManager.getConnection(url)
-        stmt = conn.createStatement()
-
-
-        #jdbc_hook = LoggingJdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
-        resultSet = stmt.executeQuery("SELECT TOP 5 * FROM DEATH")
-        while resultSet.next():
-          print(resultSet.getString(1))  # 첫 번째 컬럼의 데이터 출력
-
-        resultSet.close()
-        stmt.close()
-        conn.close()
-
-        if jpype.isJVMStarted():
-            jpype.shutdownJVM()
+        # JdbcHook 인스턴스 생성, 'MSSQL_JDBC_CONN'은 Airflow Connection ID입니다.
+        jdbc_hook = JdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
+        
+        # SQL 쿼리 실행, "DEATH" 테이블에서 상위 5개 레코드 조회
+        records = jdbc_hook.get_records("SELECT TOP 5 * FROM DEATH")
+        
+        for record in records:
+            print(record)  # 조회된 레코드 출력
 
     execute()
-
