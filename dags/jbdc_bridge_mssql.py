bc4907996752a398b94eb10823d6041f33ac60d5
xnaud <tmdtp314@naver.com>
2024-01-26 09:59:46 +0900

    S

--- dags/jbdc_bridge_mssql.py
+++ dags/jbdc_bridge_mssql.py
@@ -15,6 +15,24 @@ with DAG(
     schedule='0 0 * * *',
     catchup=False
 ) as dag:
+    
+
+    class LoggingJdbcHook(JdbcHook):
+        def get_conn(self):
+            # Airflow Connection 객체에서 connection 정보를 가져옵니다.
+            conn = self.get_connection(getattr(self, self.conn_name_attr))
+            conn_config = conn.extra_dejson
+            
+            # 'Extra' 필드에서 드라이버 클래스 이름(jclassname)을 추출합니다.
+            jclassname = conn_config.get('driver', None)
+            
+            # jclassname 값을 로깅합니다.
+            logging.info(f"JdbcHook.get_conn() - Driver class name: {jclassname}*********************************************************!!!!!!!!!!!!!!!!!!!!!!")
+
+            # JDBC Hook의 원래 get_conn() 메서드를 호출하여 Connection 객체를 반환합니다.
+            return super().get_conn()
+    
+
     @task(task_id='execute')
     def execute(**kwargs):
         print('start')
@@ -24,14 +42,10 @@ with DAG(
 
         Driver = jpype.JClass('com.microsoft.sqlserver.jdbc.SQLServerDriver')
 
-        print(Driver)
-        print('**************************************************************** Driver ****************************************************************')
-        jdbc_hook = JdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
-        print(jdbc_hook)
-        print('**************************************************************** jdbc_hook ****************************************************************')
+        jdbc_hook = LoggingJdbcHook(jdbc_conn_id="MSSQL_JDBC_CONN")
+
         records = jdbc_hook.get_records("SELECT * FROM DEATH")
-        print(records)
-        print('**************************************************************** records ****************************************************************')
+
         if records == None:
             records="sssssss"
         return records
